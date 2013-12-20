01957e6edc03e8519f99d3db6a54fc69995500ff
Joanna Koseff <jkose@comcast.net>
2013-12-19 16:07:10 -0800

    completed website

--- custom.js
+++ custom.js
@@ -1,34 +1,86 @@
-$(function() {
-	$( "#accordion" ).accordion({
+sliderInt = 1;
+sliderNext = 2;
+
+$(document).ready(function() {
+	$("#slider > img#1").fadeIn(500);
+	$("#slider h3#2").hide();
+	$("#slider h3#3").hide();
+	$("#slider h3#4").hide();
+	startSlider();
+});
+
+function startSlider() {
+	count = $("#slider > img").size();
+	
+	loop = setInterval(function() {
+	
+		if(sliderNext > count) {
+			sliderNext = 1;
+			sliderInt = 1;
+		}
+	
+		//$(".caption").hide(5);
+		//$(".caption" + sliderNext).hide(5);
+		
+		 $("#slider > img").css("display", "none");
+		 $("#slider .carousel-caption h3").css("display", "none");
+		 $("#slider .carousel-caption h3#" + sliderNext).fadeIn(500);
+		 $("#slider > img#" + sliderNext).fadeIn(500);
+		
+		sliderInt = sliderNext
+		sliderNext = sliderNext + 1;
+	
+	}, 7000)
+}
+
+function prev() {
+	newSlide = sliderInt - 1;
+	showSlide(newSlide);
+}
+
+function next() {
+	newSlide = sliderInt + 1;
+	showSlide(newSlide);
+}
+
+function stopLoop() {
+	window.clearInterval(loop);
+}
+
+function showSlide(id) {
+		stopLoop();
+		if(id > count) {
+			id = 1;
+		} else if(id < 1) {
+			id = count;
+		}
+	
+		 $("#slider > img").css("display", "none");
+		 $("#slider .carousel-caption h3").css("display", "none");
+		 $("#slider .carousel-caption h3#" + sliderNext).fadeIn(500);
+		 $("#slider > img#" + sliderNext).fadeIn(500);
+		
+		sliderInt = id
+		sliderNext = id + 1;
+		startSlider();
+}
+
+function accordion1() {
+	$("#accordion").accordion({
 		collapsible: true,
 		autoHeight: true,
 		active: false
 	});
+}
+
+$(window).ready(function() {
+	if ($("#content").height() < $(window).height()) {
+		$("#content").height($(window).height());
+	}
+	//$(".carousel-caption").hide();
+	//$(".carousel-caption").first().show();
+	accordion1();
 });
-
-//$(document).ready(function() {
-	//$("#next").on("click", function() {
-		//var currentActiveImage = $(".carousel-slide-active");
-		//var nextActiveImage = currentActiveImage.next();
-		
-		//if (nextActiveImage).length == 0) {
-			//nextActiveImage = $(".carousel-inner img").first();
-		//}
-		
-		//currentActiveImage.removeClass("carousel-slide-active").addClass("carousel-slide").css("z-index", -10);
-		//nextActiveImage.addClass("carousel-slide-active").removeClass("carousel-slide").css("z-index", 20);
-		//$(".carousel-inner img").not([currentActiveImage, nextActiveImage]).css("z-index", 5);
-		
-	//});
-//});
-
-//$('.carousel-slide').cycle({ 
-    //fx:     'scrollHorz', 
-   // speed:  'slow', 
-    //timeout: 5000, 
-    //next:   '#next', 
-    //prev:   '#prev' 
-//});
 
 function goToNewPage() {
     location=document.dropdown.navigation.options[document.dropdown.navigation.selectedIndex].value
